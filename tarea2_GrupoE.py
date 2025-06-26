8255d2acdb0d0416f2aa0fc03134891f7e5e7726
Carlos Eduardo Quesada Ramirez <carloseduardoquesadaramirez@gmail.com>
2025-06-25 22:29:19 -0600

    Se agrego advertencia en caso de seleccionar mal la opcion

--- tarea2_GrupoE.py
+++ tarea2_GrupoE.py
@@ -24,6 +24,8 @@ def main():
     print(f"Aceleración en X: {aceleracionX:.2f} m/s²")
     print(f"Aceleración en Y: {aceleracionY:.2f} m/s²")
     print(f"Magnitud de la aceleración: {magnitudAceleracion:.2f} m/s²")
+
+    graficarFuerzas(fuerzas, fxTotal, fyTotal)
     
 
 def ingresarFuerzas():
@@ -32,28 +34,32 @@ def ingresarFuerzas():
 
     for i in range(cantidadFuerzas):
         print(f"\nFuerza #{i + 1}")
-        metodoIngreso = input(
-            "¿Cómo desea ingresar esta fuerza?" \
-            "\n1. Magnitud y dirección" \
-            "\n2. Componentes Fx y Fy" \
-            "\nSeleccione una opción: ")
         
-        if metodoIngreso == "1":
-            magnitudFuerza = float(input("Ingrese la magnitud de la Fuerza N: "))
-            anguloFuerza = float(input("Ingrese el ángulo de la Fuerza en grados: "))
-            anguloRadianes = math.radians(anguloFuerza)
-            fx = magnitudFuerza * math.cos(anguloRadianes)
-            fy = magnitudFuerza * math.sin(anguloRadianes)
-        elif metodoIngreso == "2":
-            fx = float(input("Ingrese el componente Fx de la Fuerza N: "))
-            fy = float(input("Ingrese el componente Fy de la Fuerza N: "))
-        else:
-            print("Datos inválidos")
-            continue
-        
+        while True:
+            metodoIngreso = input(
+                "¿Cómo desea ingresar esta fuerza?"
+                "\n1. Magnitud y dirección"
+                "\n2. Componentes Fx y Fy"
+                "\nSeleccione una opción: ")
+
+            if metodoIngreso == "1":
+                magnitudFuerza = float(input("Ingrese la magnitud de la Fuerza N: "))
+                anguloFuerza = float(input("Ingrese el ángulo de la Fuerza en grados: "))
+                anguloRadianes = math.radians(anguloFuerza)
+                fx = magnitudFuerza * math.cos(anguloRadianes)
+                fy = magnitudFuerza * math.sin(anguloRadianes)
+                break
+            elif metodoIngreso == "2":
+                fx = float(input("Ingrese el componente Fx de la Fuerza N: "))
+                fy = float(input("Ingrese el componente Fy de la Fuerza N: "))
+                break
+            else:
+                print("Opción inválida. Por favor seleccione 1 o 2.")
+
         fuerzas.append((fx, fy))
 
     return fuerzas
+
 
 
 def calcularFuerzaNeta(fuerzas):

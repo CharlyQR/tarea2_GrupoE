9c8eb43daffd8262955abfbafdb6c40aa2aa1858
Carlos Eduardo Quesada Ramirez <carloseduardoquesadaramirez@gmail.com>
2025-06-25 22:24:22 -0600

    Proyecto funcionando, pero sin mostrar grafica

--- tarea2_GrupoE.py
+++ tarea2_GrupoE.py
@@ -1,8 +1,29 @@
 import math
-
+import matplotlib.pyplot as plot
 
 def main():
     print("Tarea #2 de un programa de cálculo de fuerza neta y aceleración")
+
+    fuerzas = ingresarFuerzas()
+
+    print("\nFuerzas ingresadas:")
+    for i, (fx, fy) in enumerate(fuerzas):
+        print(f"Fuerza #{i + 1}: Fx = {fx:.2f} N, Fy = {fy:.2f} N")
+
+    fxTotal, fyTotal, fuerzaMagnitud = calcularFuerzaNeta(fuerzas)
+
+    print("\n-----Fuerzas Neta-----")
+    print(f"Fuerza FX Total: {fxTotal:.2f} N")
+    print(f"Fuerza FY Total: {fyTotal:.2f} N")
+    print(f"Magnitud total: {fuerzaMagnitud:.2f} N")
+
+    masa = float(input("\nIngrese la masa del bloque en kilogramos: "))
+    aceleracionX, aceleracionY, magnitudAceleracion = calcularAceleracion(fxTotal, fyTotal, masa)
+
+    print("\n-----Aceleración-----")
+    print(f"Aceleración en X: {aceleracionX:.2f} m/s²")
+    print(f"Aceleración en Y: {aceleracionY:.2f} m/s²")
+    print(f"Magnitud de la aceleración: {magnitudAceleracion:.2f} m/s²")
     
 
 def ingresarFuerzas():
@@ -29,8 +50,8 @@ def ingresarFuerzas():
         else:
             print("Datos inválidos")
             continue
-
-    fuerzas.append((fx, fy))
+        
+        fuerzas.append((fx, fy))
 
     return fuerzas
 
@@ -38,7 +59,8 @@ def ingresarFuerzas():
 def calcularFuerzaNeta(fuerzas):
     sumaFX = sum(fx for fx, _ in fuerzas)
     sumaFY = sum(fy for _, fy in fuerzas)
-    return (sumaFX, sumaFY)
+    magnitudFuerza = math.sqrt(sumaFX**2 + sumaFY**2)
+    return (sumaFX, sumaFY, magnitudFuerza)
 
 
 def calcularAceleracion(fxTotal, fyTotal, masa):
@@ -47,6 +69,25 @@ def calcularAceleracion(fxTotal, fyTotal, masa):
     magnitudAceleracion = math.sqrt(aceleracionX**2 + aceleracionY**2)
     return (aceleracionX, aceleracionY, magnitudAceleracion)
 
+def graficarFuerzas(fuerzas, fxNeta, fyNeta):
+    plot.figure()
+    plot.axhline(0, color='black', linewidth=0.5)
+    plot.axvline(0, color='black', linewidth=0.5)
+
+    plot.plot(0, 0, 'ks', markersize=10, label='Bloque')
+
+    for i, (fx, fy) in enumerate(fuerzas):
+        plot.arrow(0, 0, fx, fy, head_width=0.2, length_includes_head=True, label=f'Fuerza #{i + 1}')
+
+    plot.arrow(0,0, fxNeta, fyNeta, head_width=0.3, color='red', length_includes_head=True, label='Fuerza Neta')
+
+    plot.xlabel("Eje X (N)")
+    plot.ylabel("Eje Y (N)")
+    plot.grid(True)
+    plot.legend()
+    plot.axis('equal')
+    plot.title("Diagrama de Fuerzas")
+    plot.show()
 
 
 if __name__ == "__main__":
